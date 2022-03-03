f921c2ea53f1413f20c5bec315b44372fa79a748
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-03-03 07:01:08 -0300

    Conectando com dados

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server');
-const  userSchema = require('./user/schema/user.graphql');
-const  userResolvers = require('./user/resolvers/userResolvers');
+const userSchema = require('./user/schema/user.graphql');
+const userResolvers = require('./user/resolvers/userResolvers');
+const UsersAPI = require('./user/datasource/user');
 
 const typeDefs = [userSchema];
 const resolvers = [userResolvers];
@@ -9,6 +10,11 @@ const resolvers = [userResolvers];
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    dataSources: () => {
+        return {
+            usersAPI: new UsersAPI(),
+        }
+    },
 });
 
 server.listen().then(({ url }) => {
---
+++ api/user/datasource/user.js
@@ -0,0 +1,30 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class UsersAPI extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = 'http://localhost:3000';
+    }
+
+    async getUsers() {
+        return this.get('/users');
+    }
+
+    async getUserById(id) {
+        return this.get(`/${id}`);
+    }
+
+    async createUser(user) {
+        return this.post('/', user);
+    }
+
+    async updateUser(id, user) {
+        return this.put(`/${id}`, user);
+    }
+
+    async deleteUser(id) {
+        return this.delete(`/${id}`);
+    }
+}
+
+module.exports = UsersAPI;
