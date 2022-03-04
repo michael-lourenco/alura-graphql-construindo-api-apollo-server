41a1104bafac858840ee12c841070d042f1f714d
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-03-04 08:01:12 -0300

    Criando mutations

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -7,7 +7,15 @@ const userResolvers = {
         user: (root, { id }, { dataSources }) => {
             return dataSources.usersAPI.getUserById(id); 
         },
-    }
+    },
+    Mutation: {
+        createUser: (root, user, { dataSources }) => {
+            return dataSources.usersAPI.createUser(user);
+        },
+        updateUser: (root, { id, input }, { dataSources }) => {
+            return dataSources.usersAPI.updateUser(id, input);
+        },
+    },
 };
 
 module.exports = userResolvers;
